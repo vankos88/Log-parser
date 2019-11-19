468517583941890294183d92238e58ea113a1e45
Ivan Kartsev <ivan.kartsev@kraftvaerk.com>
2019-11-19 17:47:32 +0300

    avalonia template

---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace LogParser.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
