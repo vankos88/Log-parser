f21830bd0748b74bd3fa052b2e2799b4da435386
Ivan Kartsev <ivan.kartsev@kraftvaerk.com>
2020-01-15 14:07:11 +0300

    Update Avalonia version.

--- LogParser/Program.cs
+++ LogParser/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Logging.Serilog;
+using Avalonia.ReactiveUI;
 using LogParser.Managers;
 using LogParser.ViewModels;
 using LogParser.Views;
